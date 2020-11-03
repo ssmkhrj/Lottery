f2681f2849fd9e0db6ff72b2fe44191df0e168d8
ssmkhrj <iamssmkhrj@gmail.com>
2020-11-03 23:54:40 +0530

    First commit

---
+++ src/Lottery.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import LotteryBall from "./LotteryBall";
+import "./Lottery.css";
+
+class Lottery extends Component {
+  static defaultProps = {
+    title: "Lotto",
+    numBalls: 6,
+    maxNum: 40,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { balls: Array.from({ length: this.props.numBalls }) };
+    this.generateRandomBalls = this.generateRandomBalls.bind(this);
+  }
+
+  generateRandomBalls() {
+    // So since we are using this.state inside setState we use the callback style to update state
+    this.setState((curState) => ({
+      balls: this.state.balls.map(
+        (ball) => Math.floor(Math.random() * this.props.maxNum) + 1
+      ),
+    }));
+  }
+
+  render() {
+    return (
+      <section className="Lottery">
+        <h1>{this.props.title}</h1>
+        <div>
+          {this.state.balls.map((n) => (
+            <LotteryBall num={n} />
+          ))}
+        </div>
+        <button onClick={this.generateRandomBalls}>Generate</button>
+      </section>
+    );
+  }
+}
+
+export default Lottery;
---
+++ src/LotteryBall.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./LotteryBall.css";
+
+class LotteryBall extends Component {
+  render() {
+    return <div className="LotteryBall">{this.props.num}</div>;
+  }
+}
+
+export default LotteryBall;
